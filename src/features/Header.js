079c0fade86501876729574eacc39f39b9cdcb3d
Your Name <you@example.com>
2024-02-27 13:50:50 +0530

    Headers & sidebar

---
+++ src/features/Header.js
@@ -0,0 +1,40 @@
+/** @format */
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import HeaderOption from "./HeaderOption";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import NotificationIcon from "@mui/icons-material/Notifications";
+import ChatIcon from "@mui/icons-material/Chat";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/256/174/174857.png"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://plus.unsplash.com/premium_photo-1683121366070-5ceb7e007a97?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D"
+          title="Me"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/features/Sidebar.js
@@ -0,0 +1,35 @@
+/** @format */
+
+import { Avatar } from "@mui/material";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0066/4574/3686/files/Chicago_Skyline_LinkedIn_Background_Photo.png?v=1627912075"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Ansari Rashid</h2>
+        <h4>ansari.rashid@gmail.com</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">3,944</p>
+        </div>
+      </div>
+      <div className="sidebar__stat">
+        <p>Views on post</p>
+        <p className="sidebar__statNumber">1,364</p>
+      </div>
+      <div className="sidebar__botttom">
+        <p>Recent</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
