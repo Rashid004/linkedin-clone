0f9ca7131910d42d989406a0b799d08e35128d94
Your Name <you@example.com>
2024-04-18 20:22:00 +0530

    Login

--- src/features/Login.js
+++ src/features/Login.js
@@ -1,6 +1,8 @@
 /** @format */
 import "./Login.css";
 function Login() {
+  const loginToApp = () => {};
+  const register = () => {};
   return (
     <div className="login">
       <img
@@ -10,7 +12,20 @@ function Login() {
 
       <form>
         <input placeholder="Full name (required if regestring)" type="text" />
+        <input placeholder="Profile pic URL (optional)" type="text" />
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+
+        <button type="submit" onClick={loginToApp}>
+          Sign in
+        </button>
       </form>
+      <p>
+        Not a memeber?{" "}
+        <span className="login__register" onClick={register}>
+          Register Now
+        </span>
+      </p>
     </div>
   );
 }
